dc4fe9ff5b6961a4bb7fb74aaa23a2927c76ac0f
Sergey Shabanov <emugrey@yandex.ru>
2021-04-21 11:13:22 +0300

    1-module\1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,22 @@
 function factorial(n) {
-  // ваш код...
+
+  let result = 1;
+  
+  for (let i=1; i<=n; i++){
+
+    if (n==1 || n==0){
+      break;
+    }
+
+    result *=i;    
+
+  }
+
+  alert (result);  
+
 }
+
+factorial(0); 
+factorial(1); 
+factorial(3); 
+factorial(5);
