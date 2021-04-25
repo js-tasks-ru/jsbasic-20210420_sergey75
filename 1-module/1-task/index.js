3338297b371fc503e819c4bd8204592485c9c81c
Sergey Shabanov <emugrey@yandex.ru>
2021-04-25 16:49:20 +0300

    убрал else и проверки

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,16 +3,11 @@ function factorial(n) {
 
   if (n==1 || n==0){
     return result;
-  }
-  else {   
+  }     
     for (let i=1; i<=n; i++){
       result *=i;  
+    
     }
-  }
   return result;  
 }
 
-factorial(0); 
-factorial(1); 
-factorial(3); 
-factorial(5);
