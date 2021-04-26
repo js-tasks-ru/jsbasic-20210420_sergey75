d3da7ec786b8aa74e02ddd1b3b3bc8a15cf2bd96
Sergey Shabanov <emugrey@yandex.ru>
2021-04-26 11:53:51 +0300

    1-module\1-task&2-task done

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,14 @@
-function ucFirst(str) {
-  // ваш код...
+function ucFirst(str) {  
+    if (!str){
+      return ('');
+    }    
+    let firstLetter = str[0].toUpperCase();
+    let otherWord = str.slice(1);
+    return (firstLetter + otherWord);    
 }
+
+let str = prompt('Введите слово или букву');  
+ucFirst(str);
+
+
+
