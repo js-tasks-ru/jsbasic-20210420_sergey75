d71a93c3ad9d7aab8b1deac19ca4f8726ff8c325
Sergey Shabanov <emugrey@yandex.ru>
2021-04-30 13:38:59 +0300

    2 module is done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,22 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  let sum = 0;
+  
+  for (let prop in salaries)
+    if ((typeof salaries[prop] === 'number') && isFinite(salaries[prop]) && !isNaN(salaries[prop])){
+    sum +=salaries[prop];
+    }
+  return sum;   
+}
+
+sumSalary(salaries);
+
+
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,22 @@
 let calculator = {
-  // ваш код
+  
+  read(a, b){
+    calculator.a = a;
+    calculator.b = b;
+  },
+
+  sum(){
+    return this.a+this.b;
+  },
+
+  mul() {
+    return this.a*this.b;
+  }
+
 };
+
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
