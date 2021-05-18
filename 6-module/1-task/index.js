77578fd7e223fe2c701606ae893a036a9644f928
Sergey Shabanov <emugrey@yandex.ru>
2021-05-18 16:57:50 +0300

    6-module\1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,28 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let elem = document.querySelector('tbody'); 
+
+    for (let i = 0; i < rows.length; i++){
+      this.name = rows[i].name;
+      this.age = rows[i].age;
+      this.salary = rows[i].salary;
+      this.city = rows[i].city;      
+      elem.insertAdjacentHTML('beforeend',
+         `<tr><td>${this.name}</td>
+         <td>${this.age}</td>
+         <td>${this.salary}</td>
+         <td>${this.city}</td>
+         <td><button>X</button></td></tr>`);          
+    }
+
+    let button = elem.querySelectorAll('button');  
+
+    button.forEach((value) => {
+      value.addEventListener('click', () => {
+          value.parentNode.parentNode.remove();
+        })
+    })
+
   }
 }
